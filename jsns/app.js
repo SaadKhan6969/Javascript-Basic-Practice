2efb347eaeb344cb16bbaf06a24e7e5ce0da252b
SaadKhan6969 <115664292+SaadKhan6969@users.noreply.github.com>
2023-03-02 08:04:47 +0500

    Add files via upload

---
+++ jsns/app.js
@@ -0,0 +1,354 @@
+//   var a=Math.random()
+//   var toss=Math.round(a)
+//   var toss=Math.ceil(a*6)
+//   console.log(toss)
+
+// var months=["jan","Feb","Mar","Apr","May","Jun","Jul","Aug","sept","Oct","nov","Dec"]
+
+// var dt=new Date()
+// var a=dt.getMonth()
+// var a=dt.getDay()
+// var a=dt.getDate()
+// var a=dt.getFullYear()
+// var a=dt.getHours()
+// // console.log(months[a])
+// dt.setDate(17)
+// dt.setMonth(5)
+// dt.setFullYear(2004)
+// dt.setHours(00)
+// dt.setMinutes(00)
+// dt.setSeconds(00)
+// console.log(dt)
+
+// var dt=new Date("6-17-2004")
+// var dt2=new Date()
+//  var a=dt.getTime()
+//  var b=dt2.getTime()
+
+//  var c=b-a
+// var d=Math.round(c/(1000*60*60*24*365))
+
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+// function ab(){
+//     alert("2jz")
+//     console.log("fvck it")
+// }
+
+// ab()
+
+// function abc(a,b){
+//     var amount=a
+//     var discount=b
+//     var totalamount=amount-discount
+// console.log(totalamount)
+// }
+// abc(100,20)
+// abc(200,40)
+// abc(300,60)
+// abc(400,80)
+
+// var a="huehue"
+// function abc(){
+//     console.log(a)
+// }
+// abc()
+
+// var count=0
+// function addcount(){
+//     count=1
+// }
+// addcount()
+// console.log(count)
+
+// function abc(){
+//     var a="kkko"
+// }
+// abc()
+// console.log(a)
+
+// function abc(){
+//     var a=10
+//     return a
+// }
+// var c=abc()
+// console.log(c)
+
+// function xyz(){
+//     var u=100
+//     return u
+// }
+// var e=xyz()
+// console.log(e)
+
+
+// var day="sat"
+// switch(day){
+//     case "sun":
+//         console.log("Holiday");
+//         break;
+//         case "mon":
+//             console.log("First Day");
+//             break;
+//             case "tue":
+//                 console.log("Second day");
+//                 break;
+//             default:
+//                 console.log("No day found")
+// }
+
+// i=0
+// while(i<=100){
+//     console.log(i)
+//     i++
+// }
+
+// var i=0
+// do{
+//     console.log(i)
+//     i++
+// }while(i<0)
+
+// i=0
+// while(i<10){
+//     i++
+//     console.log(i)
+// }
+
+// i=0
+// do{
+//     console.log(i)
+//     i++
+// }while(i<0)
+
+
+// function abc(a){
+//     console.log(a)
+// }
+
+// function calculator(){
+// var a=+prompt("Enter a num")
+// var b=prompt("Enter a operator")
+// var c=+prompt("Enter 2nd Numb")
+// if(b ==="+"){
+//     console.log(a+c)
+// }else if(b ==="-"){
+//     console.log(a-c)
+// }else if(b === "*"){
+//     console.log(a*c)
+// }else if(b === "/"){
+//     console.log(a/c)
+// }
+// }
+// calculator()
+
+
+// function squ(){
+//     var a=+prompt("Enter A num")
+//     var b=a**2
+//     console.log(b)
+// }
+// squ()
+
+// function counting(){
+//     var a=+prompt("Enter Starting Num")
+//     var b=+prompt("Enter Ending Num")
+//     for(i=a;i<=b;i++)
+//     console.log(i)
+// }
+// counting()
+
+// function abc(a){
+//   console.log(a)  
+// }
+// function ab(b){
+//     console.log(b)
+// }
+
+
+// var textinput=document.getElementById("txt")
+// function getvalue(){
+//     console.log(textinput.value)
+// }
+
+
+// var textinput=document.getElementById("hue")
+// var a=[]
+// function addvalue(){
+// a.push(textinput.value)
+// console.log(a)
+// }
+
+// var textinput=document.getElementById("hue")
+// var errorinput=document.getElementById("err")
+// var a=[]
+// function addvalue(){
+//     var b=textinput.value
+//     if(b===""){
+//         errorinput.value="Enter Value"
+//     }else{
+//         a.push(b)
+//         console.log(a)
+//     }
+// }
+// var textinput=document.getElementById("huee")
+// var errorpera=document.getElementById("err")
+// function addvalue(){
+//     var b=textinput.value
+//     if(b===""){
+//         errorpera.innerHTML="Enter Value"
+//     }else{
+//         console.log(b)
+//     }
+// }
+
+// var textinput=document.getElementById("huee")
+// var errorpera=document.getElementById("err")
+// function addvalue(){
+//     var b=textinput.value
+//     if(b===""){
+//         errorpera.innerHTML="Enter Value"
+//     }else{
+//         console.log(b)
+//     }
+// }
+
+// function addvalue1(){
+//     var textinp1 = document.getElementById("tx1")
+//     var valinp1 =document.getElementById("value1")
+//     var a=textinp1.value
+//     if(a===""){
+//         valinp1.innerHTML="Enter First Name"
+
+//     }else{
+//         valinp1.innerHTML=a
+//     }
+// }
+
+
+// function addvalue2(){
+//     var textinp2=document.getElementById("tx2")
+//     var valinp2=document.getElementById("value2")
+//     var b=textinp2.value
+//     if(b===""){
+//         valinp2.innerHTML="Enter Ur last Name"
+//     }else{
+//         valinp2.innerHTML=b
+//     }
+// }
+
+// function addvalue3(){
+//     var textinp3=document.getElementById("tx3")
+//     var valinp3=document.getElementById("value3")
+//     var c=textinp3.value
+//     if(c===""){
+//         valinp3.innerHTML="Enter Your Email"
+//     }else{
+//         valinp3.innerHTML=c
+//     }
+// }
+
+// function addvalue4(){
+//    var textinp4=document.getElementById("tx4")
+//    var valinp4=document.getElementById("value4")
+//    var d=textinp4.value
+//    if(d===""){
+// valinp4.innerHTML="Enter Your Password"
+//    }else{
+//     valinp4.innerHTML=d
+//    }
+// }
+
+// function addvalue5(){
+//     var textinp5=document.getElementById("tx5")
+//     var valinp5=document.getElementById("value5")
+//     var e=textinp5.value
+//     if(e===""){
+//         valinp5.innerHTML="Enter Your Cnic Number"
+//     }else{
+//         valinp5.innerHTML=e
+//     }
+// }
+// function addvalue6(){
+//     var textinp6=document.getElementById("tx6")
+//     var valinp6=document.getElementById("value6")
+//     var f=textinp6.value
+//     if(f===""){
+//     valinp6.innerHTML="Enter Your Content"    
+//     }else{
+//         valinp6.innerHTML=f
+//     }
+// }
+
+
+
+// function abc(a){
+// console.log(a)
+// a.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRQR0qmkg9AYMhbc-5rAqP8C1XkcJuH8PUgg&usqp=CAU"
+// }
+// function ab(a){
+//     console.log(a)
+//     a.src="https://2.bp.blogspot.com/-pp195TBk9kM/X9bVF5XfdGI/AAAAAAAABFA/OzmP23lzSMYRtPKFku_tqqlFNsm55CL9gCLcBGAsYHQ/w400-h239/MK4-paul-shworoomotommotif.jpg"
+// }
+
+
+
+// function abc(a){
+// console.log(a)
+// a.className ="bigbox"
+// }
+
+// var student = {
+//     rollno: 2269,
+//     name: "Saad Ayaz Khan",
+//     course: "Snfc",
+//     age: 18
+// }
+// student.name = "Ahmed"
+// student.inst = "Saims"
+// delete student.name
+// var a = "name" in student
+
+
+// console.log(student)
+// console.log(a)
+
+// var obj = {
+//         rollno: 2269,
+//         name: "Saad Ayaz Khan",
+//         course: "Snfc",
+//         age: 18,
+//         address:{
+//             houseNo:37,
+//             Street:13,
+//            area:"Hawksbay"
+//         },
+//         abc: function(){
+//             console.log("Ghar Mill gya")
+//         }
+//     }
+//    obj.abc()
+// console.log(obj)
+
+
+// var firstname=document.getElementById('firstname')
+// var lastname=document.getElementById('lastname')
+// var email=document.getElementById('email')
+// var obj = {}
+// function search(){
+//   obj.firstname=firstname.value
+//   obj.lastname=lastname.value
+//   obj.email=email.value
+//   console.log(obj)
+// }
+
+// var student = {
+//   name:prompt("Enter Name"),
+//   rollno:prompt("ENter Rollno")
+
+// }
+// console.log(student)
